a2c13ac7d7bb16ebfa3a8e4e4d907869a70a1889
ccn <ccn@cuppajoeman.com>
2023-10-17 12:06:26 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -7,7 +7,7 @@ for filename in glob('../**/project.godot', recursive=True):
     if filename == "../project.godot":
         continue # we must leave the root project file
 
-    storage_filename = f"project_godot_files/{filename[3:]}"
+    storage_filename = f"project.godot_files/{filename[3:]}"
     storage_dirname = os.path.dirname(storage_filename)
 
     os.makedirs(storage_dirname, exist_ok=True)
